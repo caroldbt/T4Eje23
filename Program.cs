67db371c9ece9a790feee7d376d6827c48f3e68e
caroldbt <37454302+caroldbt@users.noreply.github.com>
2021-07-10 00:42:40 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace CalculadoraInversa
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese un numero: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese uno de estos signos (+,-,*,/,^,%)");
+            string signo = Console.ReadLine();
+            double total = 0;
+            switch (signo)
+            {
+                case "+": total = num1 + num2; break;
+                case "-": total = num1 - num2; break;
+                case "*": total = num1 * num2; break;
+                case "/": total = (double)num1 / (double)num2; break;
+                case "^":
+                    total = Math.Pow(num1, num2); break;
+                case "%": total = num1 % num2; break;
+                default:
+                    Console.WriteLine("Opcion incorrecta."); break;
+            }
+            string resultado = num1 + signo + num2 + " = " + total;
+
+            Console.WriteLine(resultado);
         }
     }
 }
